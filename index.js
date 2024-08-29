e7ac54160d6f89854d0b305f15ce387a3f9f7cc5
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-08-29 14:12:21 +0200

    core: Add light/dark mode toggle functionality and update styling

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const bgColorLight = "hsl(0, 0%, 96%)";
+const textColorLight = "hsl(0, 0%, 20%)";
+const accentColorLight = "hsl(281, 78%, 30%)";
 
+const bgColorDark = "hsl(0, 0%, 10%)";
+const textColorDark = "hsl(0, 0%, 80%)";
+const accentColorDark = "hsl(281, 78%, 70%)";
+
+const root = document.documentElement;
+let isDarkMode = false;
+
+const toggleLightDarkMode = () => {
+  console.log("currVal", isDarkMode);
+
+  // Change the values of the CSS variables
+  root.style.setProperty("--bg-color", isDarkMode ? bgColorLight : bgColorDark);
+  root.style.setProperty(
+    "--text-color",
+    isDarkMode ? textColorLight : textColorDark
+  );
+  root.style.setProperty(
+    "--accent-color",
+    isDarkMode ? accentColorLight : accentColorDark
+  );
+
+  isDarkMode = !isDarkMode;
+};
