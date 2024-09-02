df3561956db174c04176a3eb8a92b5f7758863af
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-09-02 13:46:02 +0200

    core: Work on Error handling and a bit of cleaning up

--- index.js
+++ index.js
@@ -10,26 +10,39 @@ const getProducts = async () => {
 
   try {
     const response = await fetch(`${url}${queryParams}`);
+    console.log("response", response);
     if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
+      throw new Error(`${response.status}: ${response.statusText}`);
     }
     const data = await response.json();
     console.log(data);
     products = data.products;
   } catch (err) {
     console.error(err);
-    error = err;
+    error = err.message;
   }
+};
+
+const constructSlideContent = (product) => {
+  const slideContentElement = document.createElement("div");
+  slideContentElement.className = "slide-content";
+  slideContentElement.style.backgroundImage = `url(${product.thumbnail})`;
+  slideContentElement.style.backgroundSize = "contain";
+  slideContentElement.style.backgroundPosition = "right";
+  slideContentElement.style.backgroundRepeat = "no-repeat";
+  slideContentElement.style.background;
+  slideContentElement.innerHTML = `<h3 class="product-title">${product.title}</h3>`;
+
+  return slideContentElement;
 };
 
 const constructProductSlide = (product) => {
   const productSlideElement = document.createElement("li");
   productSlideElement.className = "slide-container";
-  productSlideElement.innerHTML = `
-    <div class="slide-content">
-      <h3 class="product-title">${product.title}</h3>
-    </div>
-  `;
+
+  const slideContentElement = constructSlideContent(product);
+  productSlideElement.appendChild(slideContentElement);
+
   return productSlideElement;
 };
 
@@ -47,8 +60,41 @@ const toggleLoading = () => {
   skeletonContainerElement.style.display = isLoading ? "block" : "none";
 };
 
+const constructErrorMsg = (error) => {
+  const errorMsgElement = document.createElement("p");
+  errorMsgElement.className = "error-msg";
+  errorMsgElement.textContent = error;
+  return errorMsgElement;
+};
+
+const showError = (error) => {
+  const errorMsgElement = constructErrorMsg(error);
+  const errorContainerElement = document.getElementById("error-container");
+  errorContainerElement.style.display = "flex";
+  errorContainerElement.appendChild(errorMsgElement);
+};
+
+const hideError = () => {
+  const errorContainerElement = document.getElementById("error-container");
+  errorContainerElement.style.display = "none";
+};
+
+const setSliderDisplayTo = (displayProp) => {
+  const sliderElement = document.getElementById("slider");
+  sliderElement.style.display = displayProp;
+};
+
 const boot = async () => {
   await getProducts();
+
+  if (error) {
+    setSliderDisplayTo("none");
+    toggleLoading();
+    showError(error);
+    return;
+  }
+
+  setSliderDisplayTo("flex");
   const productSlides = products.map((product) => {
     return constructProductSlide(product);
   });
